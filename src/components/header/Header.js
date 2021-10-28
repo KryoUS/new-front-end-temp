a295ec15e13815bd320f531030fd25f772833a9a
KryoUS <taurussho89@gmail.com>
2021-10-28 04:39:05 -0600

    Added Quotes to Header. Made Header mobile friendly.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -12,7 +12,6 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import Assessment from '@material-ui/icons/Assessment';
 import Hidden from '@material-ui/core/Hidden';
 import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Container from '@material-ui/core/Container';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
@@ -22,9 +21,11 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import { ReactComponent as DiscordIcon } from '../icons/Discord-Logo-White.svg';
+import { ReactComponent as ComplexityLogo } from '../icons/complexityMoose.svg';
 import Discord from './Discord';
 import RealmStatus from './RealmStatus';
 import TokenPrice from './TokenPrice';
+import Quotes from './Quotes';
 
 class Header extends React.Component {
     constructor() {
@@ -54,71 +55,125 @@ class Header extends React.Component {
 
     render(){
         return(
-            <Appbar position="fixed" color="transparent" style={{color: "#fff", boxShadow: "none"}}>
-                <Toolbar>
-                    <Hidden mdUp>
-                        {/* MOBILE/TABLET ONLY */}
+            <Appbar position="sticky">
+                <Toolbar variant={"dense"} style={{backgroundColor: "#121212"}}>
+                    <Hidden lgUp>   {/* MOBILE/TABLET ONLY */}
                         <IconButton onClick={() => this.drawerToggle()} >
                             <MenuIcon style={{fontSize: 48}} />
                         </IconButton>
-                        <Container style={{flexGrow: 1}}>
+                        <Container style={{display: "flex", justifyContent: "center", flexGrow: 1}}>
+                            <ComplexityLogo style={{height: "41px", width: "41px"}} />
                             <Typography variant="h4" align="center">Complexity</Typography>
                             {/* <img style={{height: "46px"}} src="https://res.cloudinary.com/complexityguild/image/upload/v1535585345/wow/logos/logov3.png" alt="Complexity Logo"/> */}
                         </Container>
+                        <IconButton
+                            onClick={this.handleMenu}
+                            edge={'end'}
+                            disabled
+                        >
+                            <AccountCircle style={{fontSize: 46}} />
+                        </IconButton>
                     </Hidden>
-                    <Hidden smDown>
-                        {/* DESKTOP ONLY */}
-                        <Container style={{flexGrow: 1}}>
-                            <Typography variant="h4" align="left">Complexity</Typography>
-                            {/* <img style={{height: "46px"}} src="https://res.cloudinary.com/complexityguild/image/upload/v1535585345/wow/logos/logov3.png" alt="Complexity Logo"/> */}
-                        </Container>
-                        <Grid container direction="column">
-                            <Grid item xs>
-                                <Grid container justifyContent="flex-end">    
-                                    <ButtonGroup variant="text">
-                                        <Button size="large">Home</Button>
-                                        <Button size="large">About</Button>
-                                        <Button size="large" onClick={() => this.discordToggle()}>Discord</Button>
-                                        <Button size="large">Simulations</Button>
-                                    </ButtonGroup>
-                                </Grid>
+                    <Hidden mdDown> {/* DESKTOP ONLY */}
+                        <Grid container>
+                            <Grid item xs={7} style={{display: "flex", justifyContent: "flex-start"}}>
+                                <Quotes />
                             </Grid>
-                            <Grid item xs={12}>
-                                <Grid container direction="row" justifyContent="flex-end" alignItems="center" spacing={1}>
-                                    <Grid item>
-                                        <RealmStatus />
-                                    </Grid>
-                                    <Grid item>
-                                        <Button 
-                                        variant="outlined" 
-                                        size="small"
-                                        startIcon={<Info />}>
-                                            Class Info
-                                        </Button>
-                                    </Grid>
-                                    <Grid item>
-                                        <Button 
-                                        variant="outlined" 
-                                        size="small"
-                                        startIcon={<DateRange />}>
-                                            Mythic+ Affixes
-                                        </Button>
-                                    </Grid>
-                                    <Grid item>
-                                        <TokenPrice />
-                                    </Grid>
+                            <Grid container spacing={2} item xs={5} style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                <Grid item xs={3} container spacing={2} style={{display: "flex", justifyContent: "space-evenly", alignItems: "center"}}>
+                                    <RealmStatus />
+                                </Grid>
+                                <Grid item>
+                                    <Button 
+                                    variant="text" 
+                                    size="medium"
+                                    startIcon={<Info />}>
+                                        Class Info
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button 
+                                    variant="text" 
+                                    size="medium"
+                                    startIcon={<DateRange />}>
+                                        Mythic+ Affixes
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <TokenPrice />
                                 </Grid>
                             </Grid>
                         </Grid>
                     </Hidden>
-                    <IconButton
-                        onClick={this.handleMenu}
-                        edge={'end'}
-                        disabled
-                    >
-                        <AccountCircle style={{fontSize: 48}} />
-                    </IconButton>
                 </Toolbar>
+                <Hidden mdDown> {/* DESKTOP ONLY */}
+                    <Toolbar>
+                        <Grid container align-items={"center"}>
+                            <Grid container item xs={3} zeroMinWidth style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                <Grid item>
+                                    <ComplexityLogo style={{height: "80px", width: "80px"}} />
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant="h3">Complexity</Typography>
+                                    {/* <img style={{height: "46px"}} src="https://res.cloudinary.com/complexityguild/image/upload/v1535585345/wow/logos/logov3.png" alt="Complexity Logo"/> */}
+                                </Grid>
+                            </Grid>
+                            <Grid container spacing={2} item xs={9} zeroMinWidth style={{display: "flex", justifyContent: "flex-end", alignItems: "center"}}>
+                                <Grid item>
+                                    <Button size="large">
+                                        <Typography variant="h6">News</Typography>
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button size="large" disabled>
+                                        <Typography variant="h6">About</Typography>
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button size="large" disabled>
+                                        <Typography variant="h6">Raiders</Typography>
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button size="large" disabled>
+                                        <Typography variant="h6">Members</Typography>
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button size="large" disabled>
+                                        <Typography variant="h6">Simulations</Typography>
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button size="large" disabled>
+                                        <Typography variant="h6">Twitch</Typography>
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button size="large" disabled>
+                                        <Typography variant="h6">Youtube</Typography>
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button size="large" onClick={() => this.discordToggle()}>
+                                        <Typography variant="h6">Discord</Typography>
+                                    </Button>
+                                </Grid>
+                                <Grid item style={{display: "flex", alignItems: "center", justifyContent: "flex-end"}}>
+                                    <IconButton
+                                        onClick={this.handleMenu}
+                                        edge={'end'}
+                                        disabled
+                                    >
+                                        <AccountCircle style={{fontSize: 46}} />
+                                    </IconButton>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Toolbar>
+                    {/* <Toolbar variant={"dense"} style={{backgroundColor: "#121212"}}>
+                    </Toolbar> */}
+                </Hidden>
                 <Drawer anchor="left" open={this.state.drawerOpen} onClose={this.drawerToggle} >
                     <List>
                         {/* MAKE PROGRAMATIC */}
