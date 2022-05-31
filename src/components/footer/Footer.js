7cfe08ed74c3d8a1051a610868f6df122d870909
KryoUS <taurussho89@gmail.com>
2022-05-31 01:37:40 -0600

    Added News and About links. Added Complexity logo.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import MythicAffixes from '../header/MythicPlus/MythicAffixes';
 import Discord from '../header/Discord';
-import { Grid, Typography, Button, Container, Divider, Box, Paper } from '@material-ui/core';
+import { Grid, Typography, Button, Container, Divider, Box, Paper, Hidden } from '@material-ui/core';
+import {Link as RouterLink} from 'react-router-dom';
+import { ReactComponent as ComplexityLogo } from '../icons/complexityMoose.svg';
 
 export default class News extends React.Component{
     constructor() {
@@ -25,22 +27,22 @@ export default class News extends React.Component{
                 <Container>
                     <Box sx={{padding: 12}}>
                         <Grid container>
-                            <Grid item xs={3} container justifyContent='center' alignItems='center'>
+                            <Grid item xs container justifyContent='center' alignItems='center'>
                                 <Grid item>
                                     <MythicAffixes iconHW={32} iconSize={'medium'} />
                                 </Grid>
                             </Grid>
-                            <Grid item xs={3} container direction="column">
+                            <Grid item xs container direction="column">
                                 <Grid item>
                                     <Typography variant="h6">NAVIGATION</Typography>
                                 </Grid>
                                 <Grid item>
-                                    <Button size="small">
+                                    <Button size="small" component={RouterLink} to={"/"}>
                                         <Typography variant="body1">News</Typography>
                                     </Button>
                                 </Grid>
                                 <Grid item>
-                                    <Button size="small" disabled>
+                                    <Button size="small" component={RouterLink} to={"/about"}>
                                         <Typography variant="body1">About</Typography>
                                     </Button>
                                 </Grid>
@@ -73,6 +75,13 @@ export default class News extends React.Component{
                                     <Button size="small" onClick={() => this.discordToggle()}>
                                         <Typography variant="body1">Discord</Typography>
                                     </Button>
+                                </Grid>
+                            </Grid>
+                            <Grid item xs container justifyContent='center' alignItems='center'>
+                                <Grid item>
+                                    <Hidden mdDown> {/* DESKTOP ONLY */}
+                                        <ComplexityLogo style={{height: "200px"}} />
+                                    </Hidden>
                                 </Grid>
                             </Grid>
                         </Grid>
