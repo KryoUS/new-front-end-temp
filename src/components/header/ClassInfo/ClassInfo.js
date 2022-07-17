0f6cfd6fbf7dce19a79b1ca947dc0123b32decfc
KryoUS <taurussho89@gmail.com>
2022-07-17 07:16:35 -0600

    Removed restrictive spacing for better look.

--- src/components/header/ClassInfo/ClassInfo.js
+++ src/components/header/ClassInfo/ClassInfo.js
@@ -11,7 +11,7 @@ export default function ClassInfo(props){
                 <Box sx={{padding: 12}}>
                     <Grid container spacing={4} style={{ justifyContent: "center" }}>
                             {ClassInfoData.map(classObj => {
-                                return <Grid key={classObj.className + ' Spec Info'} item container sm={6} md={4} lg={3} xl={2} style={{ width: "300px" }}>
+                                return <Grid key={classObj.className + ' Spec Info'} item container sm={6} md={4} lg={3} style={{ width: "300px" }}>
                                     <ClassInfoList classObj={classObj} />
                                 </Grid>
                             })}
